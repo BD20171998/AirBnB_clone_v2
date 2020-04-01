6e699ec466d1797a681e7f4f8410d7e7a99d7353
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-01 00:31:42 +0000

    Adding updated State class

--- models/state.py
+++ models/state.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 """This is the state class"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-class State(BaseModel):
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
+        state_id: The state id
         name: input name
     """
-    name = ""
+
+    """This init method allows the class to access BaseModel's attributes"""
+    def __init__(self):
+        BaseModel.__init__(self)
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete",)
+
+    @property
+    def cities(self):
+        """
+        Returns the list of City instances with state_id equals to the current
+        State.id as private
+        """
+        #session.query(State, City).filter(City.state_id == State.id).all()
+
+        results = self.storage.all()
+        print(results)
+        return self.__results
