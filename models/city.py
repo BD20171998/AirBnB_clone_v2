a4e2b0224dccf9c7c2bef49e5636439c1a6c2bf4
christinepang1 <christinezpang@gmail.com>
2020-04-01 20:55:54 +0000

    Implement DBStorage for Place, backrefs to user and city

--- models/city.py
+++ models/city.py
@@ -2,9 +2,11 @@
 """This is the city class"""
 from models.base_model import BaseModel
 from models.base_model import Base
-#from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from models.state import State
+from sqlalchemy.orm import relationship
+
+
 class City(BaseModel, Base):
     """This is the class for City
     Attributes:
@@ -12,6 +14,8 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = "cities"
-    
+
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship("Place", backref="cities", cascade="all, delete")
