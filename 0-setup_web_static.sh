19b8a7f8bf9bc2c32ad043a95410014d074ed508
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-07 00:08:48 +0000

    Changed html for index file and changed command from ls to ln

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,14 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "<html>Testing 123</html>" | sudo tee /data/web_static/releases/test/index.html           
-sudo ls -sf /data/web_static/releases/test/ /data/web_static/current
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default                                                    
 sudo service nginx restart
